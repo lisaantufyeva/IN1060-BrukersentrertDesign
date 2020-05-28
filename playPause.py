fd3b43d13b848e7540ee8d0a9eb10f5ef127669e
Lisa Antufyeva <lisa.antufyeva@gmail.com>
2020-05-28 07:00:26 +0200

    spiller av lyd, men venter på input for å spille av neste. Pauser og resume track, men fortsetter ikke til netse fil i listen

--- playPause.py
+++ playPause.py
@@ -4,6 +4,7 @@ import pygame
 
 NEXT = pygame.USEREVENT+1
 
+
 currenttrack = 0
 
 workout1nivaa1 = ["/share/skolearbeid/media/intro0101.mp3","/share/skolearbeid/media/lyd0101.mp3"]
@@ -12,9 +13,11 @@ paused = False
 
 
 
+
 def main():
 
     playWorkout(workout1nivaa1, currenttrack)
+
 
 def playWorkout(lst, currenttrack):
 
@@ -29,20 +32,63 @@ def playWorkout(lst, currenttrack):
     pygame.mixer.music.play()
     pygame.mixer.music.set_endevent(NEXT)
 
+
+
     running = True
-    #inpP = input("pause? ")
+
 
     while running:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
             elif event.type == NEXT:
+
                 currenttrack = (currenttrack+1) % antall_filer
                 print ("Play: ", lst[currenttrack])
                 pygame.mixer.music.load(lst[currenttrack])
                 pygame.mixer.music.play()
 
 
+
+        inp = input("pause ?")
+        if (inp == "p"):
+            pause();
+
+
+            paused = True
+            inp = input("resume? ")
+            if (inp =="p"):
+                paused = False
+                pygame.mixer.music.unpause()
+
+
+
+
+
+
 pygame.quit()
+
+def pause():
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.pause()
+
+
+def settPause():
+
+    paused = True
+    pygame.mixer.music.pause()
+    while (paused):
+        inpP = input("unpause? ")
+        if(inpP == "P"):
+            paused = False
+            setUnpause()
+
+def settUnpause():
+    paused = False
+    pygame.mixer.music.unpause()
+    #playWorkoutLoop(lst, currenttrack)
+
 
 main()
