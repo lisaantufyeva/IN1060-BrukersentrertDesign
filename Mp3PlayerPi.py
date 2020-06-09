f41b4bc939d4bf187fd7ad83e4c5bc43f5f98c88
Liza Antufyeva <elizavetaantufyeva@MacBook-Air.local>
2020-06-10 00:18:08 +0200

    added comments to Mp3PlayerPi

--- Mp3PlayerPi.py
+++ Mp3PlayerPi.py
@@ -15,24 +15,29 @@ workout2nivaa1 = ["lydfiler/111pushups.mp3","lydfiler/112pushups.mp3", "lydfiler
 workout2nivaa2 = ["lydfiler/211Rygghev.mp3","lydfiler/212Rygghev.mp3","lydfiler/221Baten.mp3", "lydfiler/222Baten.mp3", "lydfiler/231Utfall.mp3", "lydfiler/232Utfall.mp3", "lydfiler/241Planken.mp3", "lydfiler/242Planken.mp3", "lydfiler/251Sideplanke.mp3", "lydfiler/252Sideplanke.mp3", "lydfiler/253Sideplanke2.mp3", "lydfiler/261Kneboy.mp3", "lydfiler/262Kneboy.mp3", "lydfiler/271Benhev.mp3", "lydfiler/272Benhev.mp3", "lydfiler/281Pushups.mp3", "lydfiler/282Pushups.mp3" ]
 workout2nivaa3 = []
 
-
+#setter opp serial
 ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
 ser.baudrate = 115200
 
+#lager en event som gjør at neste lydspor spilles av automatisk
 NEXT = pygame.USEREVENT+1
+
+# holder styr på spillelisten og lydfil som avspilles
 currentplaylist = []
 currenttrack = 0
 
 def main():
-
+    #initierer pygame
     pygame.init()
     pygame.mixer.init(frequency = 48000)
 
     while True:
         print("loop")
         read_ser = ser.readline()
+        #kaller på metoden som spiller av neste lydfil
         handlePygameEvents()
 
+        #leser meldingener som kommer via serial
         message = read_ser.decode("ASCII")
         if message != "":
             parts = message.strip().split(" ")
@@ -42,11 +47,10 @@ def main():
             commando = parts[0]
             workout = parts[1]
             level = parts[2]
-
+            #kaller på funksjonene som tolker meldingen
             commandfraArduino(commando, workout, level)
 
-            #playWorkout(commando, workout, level)
-
+# funksjonen kaller på en relevant funksjon basert meldingen
 def commandfraArduino(commando, workout, level):
     if (commando == "PLAY"):
         playWorkout(commando, workout, level)
@@ -61,7 +65,7 @@ def commandfraArduino(commando, workout, level):
 
 
 
-
+#funksjonen velger ut riktig spilleliste
 
 def playWorkout(commando, workout, level):
     global currentplaylist
@@ -86,7 +90,7 @@ def playWorkout(commando, workout, level):
     currenttrack = 0
     spillWorkout()
 
-
+#funksjonen spiller av liste
 
 def spillWorkout():
     if len(currentplaylist) > 0 and  currenttrack < len(currentplaylist):
@@ -95,12 +99,14 @@ def spillWorkout():
         if playlistNotEmpty():
             pygame.mixer.music.set_endevent(NEXT)
 
-
+# sjekker om listen er tom
 def playlistNotEmpty():
     return currenttrack < len(currentplaylist)-1
-
+#sjekker om lydfilen er starten på spilleliste
 def notFirstTrack():
     return len(currentplaylist) - currenttrack <= len(currentplaylist)
+
+#funksjonen spiller av neste lydfil automatisk
 def handlePygameEvents():
     global currenttrack
     for event in pygame.event.get():
@@ -109,28 +115,28 @@ def handlePygameEvents():
             currenttrack = (currenttrack + 1)
             spillWorkout()
 
+#funksjonen spiller av neste lydfil dersom bruker hopper frem i spillelisten
 def playNext():
     global currenttrack
     if playlistNotEmpty():
         currenttrack = (currenttrack + 1)
         spillWorkout()
-
+#funksjonen spiller av forrige lydfil dersom bruker hopper tilbake i spillelisten
 def playPrev():
     global currenttrack
     if notFirstTrack():
         currenttrack = (currenttrack - 1)
         spillWorkout()
-
+#funksjonen pauser avspilling
 def pause():
     #if pygame.mixer.music.get_busy():
         pygame.mixer.music.pause()
         print("paused")
+#funskjonen resumer avspilling
 def resume():
     pygame.mixer.music.unpause()
     print("resume")
 
-def stopLyd():
-    return
 
 
 main()
